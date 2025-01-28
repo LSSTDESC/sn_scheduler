e9e922de64ecc418a013202d6f9662350b9533e6
pgris <gris@clermont.in2p3.fr>
2025-01-28 09:04:05 +0100

    add a setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# get the version here
+pkg_vars = {}
+
+with open("version.py") as fp:
+    exec(fp.read(), pkg_vars)
+
+setup(
+    name='sn_scheduler',
+    version=pkg_vars['__version__'],
+    description='Set of tools used to run SN pipeline',
+    url='http://github.com/lsstdesc/sn_scheduler',
+    author='Philippe Gris',
+    author_email='philippe.gris@clermont.in2p3.fr',
+    license='BSD',
+    packages=['sn_scheduler'],
+    # All files from folder sn_script_input
+    # package_data={'sn_script_input': ['*.txt']},
+    python_requires='>=3.5',
+    zip_safe=False
+)
